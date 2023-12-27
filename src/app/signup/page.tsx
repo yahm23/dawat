8e906ab0609200dae252534696929b030e788125
Yousef <yahmed1996a@gmail.com>
2023-12-27 17:22:48 +0000

    Add signup page and

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,21 @@
+'use client';
+import React, { useEffect } from 'react';
+import { useAppSelector } from '../../lib/hooks'
+import { AuthRouter } from '@/components/authRouter';
+
+
+const Signup = () => {
+  const loggedStatus = useAppSelector(state => state.authReducer.loggedIn)
+
+  useEffect(()=>{
+    return () => AuthRouter({loggedStatus})
+  },[loggedStatus]);
+  
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Signup
