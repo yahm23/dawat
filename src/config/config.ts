1b1cbf4131569560c3373b91dd121f23be63a76e
Yousef <yahmed1996a@gmail.com>
2023-12-28 18:45:15 +0000

    Move firebase credentials to .env and update config

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 
 export const config = {
     firebase: {
-        apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-        appId: process.env.REACT_APP_FIREBASE_ID,
-        authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-        messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-        projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-        storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-        measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+        apiKey: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_API_KEY,
+        appId: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_ID,
+        authDomain: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_AUTH_DOMAIN,
+        messagingSenderId: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+        projectId: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_STORAGE_BUCKET,
+        measurementId: process.env.NEXT_PUBLIC_REACT_APP_MEASUREMENT_ID,
     }
 }
 
