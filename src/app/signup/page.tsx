66aa508cb29888ce13df9d51019675ccce9147cc
Yousef <yahmed1996a@gmail.com>
2023-12-28 19:00:34 +0000

    Create email signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,21 +1,77 @@
 'use client';
-import React, { useEffect } from 'react';
-import { useAppSelector } from '@/lib/hooks';
-import { AuthRouter } from '@/components/authRouter';
+import React, { useState } from 'react';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import {auth} from '@/config/config';
+import { setLoggedIn, setUserEmail, setUserId } from '@/lib/slices/authSlice';
+
+const SignIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [
+    createUserWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useCreateUserWithEmailAndPassword(auth);
+
+  const newUserMiddleware = async (email:string, password:string) => {
+    const result = await createUserWithEmailAndPassword(email, password);
+    
+    if (result) {
+        setLoggedIn(true);
+        setUserEmail(result.user.email ? result.user.email : '');
+        setUserId(result.user.uid ? result.user.uid : '');
+    }
+  }
+
+  if (error) {
+    console.log(error.code);
+    switch(error.code) {
+        case 'auth/email-already-in-use':
+            return (
+                <div>
+                    <p>Oops! This email is already in use, please login or reset your password.</p>
+                </div>
+            );
+          break;
+        case 'auth/credential-already-in-use':
+          return (
+              <div>
+                  <p>Oops! These creditionals are already in use, please login, or reset your account.</p>
+              </div>
+          );
+          break;
+        default:
+            return (
+                <div>
+                    <p>Oops! Something went wrong, try again in a bit.</p>
+                </div>
+            );
+      }
+  }
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
 
-const Signup = () => {
-  const loggedStatus = useAppSelector(state => state.authReducer.loggedIn)
-
-  useEffect(()=>{
-    return () => AuthRouter({loggedStatus})
-  },[loggedStatus]);
-  
   return (
-    <div>
-        
+    <div className="App">
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={() => newUserMiddleware(email, password)}>
+        Register
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default SignIn;
